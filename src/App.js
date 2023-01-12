a72907788d91b04b792f9da9257e76c1a5806168
Lubna Tahreem <lubnatahreem789@gmail.com>
2023-01-12 15:34:40 +0530

    Startinf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { createContext, useContext, useState } from 'react';
+
+
+function C4(){
+  const [va,va2] = useContext(Env);
+
+  return(
+    <div>
+        <p>hi{va}{va2}</p>
+    </div>
+  )
+}
+const Env = createContext();
 
 function App() {
+  const [set,setShow]= useState("Lubna");
+  const [set2,setShow2]= useState("Tahreem");
   return (
+   <Env.Provider value={[set,set2]}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {set}{set2}</h1>
+      <C4></C4>
     </div>
+    </Env.Provider>
   );
 }
 
